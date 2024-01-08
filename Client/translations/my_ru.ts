62a6eb7ed60fd5e33e1a15d7ed1255d2cf1b27b8
kab1201 <125040829+kab1201@users.noreply.github.com>
2024-01-08 22:12:37 +0300

    Add files via upload

---
+++ Client/translations/my_ru.ts
@@ -0,0 +1,396 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>ConnectForm</name>
+    <message>
+        <location filename="../sources/connectform.ui" line="20"/>
+        <source>Form</source>
+        <translation>Форма подключения к серверу</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.ui" line="33"/>
+        <source>IP address:</source>
+        <translation>IP-адрес:</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.ui" line="43"/>
+        <source>Port:</source>
+        <translation>Порт:</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.ui" line="67"/>
+        <location filename="../sources/connectform.cpp" line="25"/>
+        <location filename="../sources/connectform.cpp" line="56"/>
+        <source>Connect</source>
+        <translation>Подключение</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.ui" line="87"/>
+        <source>Exit</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.cpp" line="34"/>
+        <location filename="../sources/connectform.cpp" line="42"/>
+        <source>Client</source>
+        <translation>Клиент</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.cpp" line="35"/>
+        <source>Runtime Out</source>
+        <translation>Истекло время ожидания</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.cpp" line="43"/>
+        <source>Successful Connection</source>
+        <translation>Успешное подключение</translation>
+    </message>
+    <message>
+        <location filename="../sources/connectform.cpp" line="47"/>
+        <source>Disconnect</source>
+        <translation>Отключение</translation>
+    </message>
+</context>
+<context>
+    <name>LoginForm</name>
+    <message>
+        <location filename="../sources/loginform.ui" line="20"/>
+        <source>Form</source>
+        <translation>Форма входа</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="28"/>
+        <source>Login</source>
+        <translation>Пользователь:</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="38"/>
+        <source>Password</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../sources/loginform.ui" line="98"/>
+        <source>Registration</source>
+        <translation>Регистрация</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Чат</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="36"/>
+        <source>Your message:</source>
+        <translation>Ваше сообщение:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="56"/>
+        <source>Send</source>
+        <translation>Отправить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="77"/>
+        <source>Information</source>
+        <translation>Информационные сообщения:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="116"/>
+        <source>Main menu</source>
+        <translation>Меню</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="126"/>
+        <location filename="../sources/mainwindow.ui" line="129"/>
+        <source>Open another client</source>
+        <translation>Открыть новое окно чата</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="134"/>
+        <source>Close this client</source>
+        <translation>Закрыть окно</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="27"/>
+        <location filename="../sources/mainwindow.cpp" line="303"/>
+        <source>Current user:</source>
+        <translation>Текущий пользователь:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="83"/>
+        <source>Can&apos;t Send Empty Message</source>
+        <translation>Нельзя отправить пустое сообщение!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="91"/>
+        <source>You need to select a user!</source>
+        <translation>Необходимо выбрать пользователя!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="105"/>
+        <location filename="../sources/mainwindow.cpp" line="246"/>
+        <location filename="../sources/mainwindow.cpp" line="355"/>
+        <source>write to all:</source>
+        <translation>пишет всем:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="127"/>
+        <location filename="../sources/mainwindow.cpp" line="251"/>
+        <location filename="../sources/mainwindow.cpp" line="377"/>
+        <source>write to</source>
+        <translation>пишет</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="199"/>
+        <location filename="../sources/mainwindow.cpp" line="215"/>
+        <source>Current user: </source>
+        <translation>Текущий пользователь: </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="215"/>
+        <source> (in ban)</source>
+        <translation> (забанен)</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="399"/>
+        <source>left the Chat!</source>
+        <translation>покинул Чат!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="412"/>
+        <source>You are in BAN!</source>
+        <translation>Вы забанены!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="398"/>
+        <location filename="../sources/mainwindow.cpp" line="420"/>
+        <location filename="../sources/mainwindow.cpp" line="437"/>
+        <source>User with login </source>
+        <translation>Пользователь с логином </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="429"/>
+        <source>You are UNBAN!</source>
+        <translation>Вы разбанены!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="291"/>
+        <source>CAUTION</source>
+        <translation>ВНИМАНИЕ</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="292"/>
+        <source>You are dissconnect from server!</source>
+        <translation>Вы отключены от сервера!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="341"/>
+        <source>join to Chat!</source>
+        <translation>присоединился к Чату!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="422"/>
+        <source>sent to ban!</source>
+        <translation>отправлен в бан!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="439"/>
+        <source>Unban!</source>
+        <translation>разбанен!</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="271"/>
+        <source>Logout user</source>
+        <translation>Выход пользователя из системы</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="272"/>
+        <source>You have logged out of the chat</source>
+        <translation>Вы вышли из чата</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="277"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="278"/>
+        <source>Error send message</source>
+        <translation>Ошибка при отправке сообщения</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="97"/>
+        <source>Registration</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="98"/>
+        <source>Registration of a new user was successful!</source>
+        <translation>Регистрация нового пользователя прошла успешно!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="130"/>
+        <source>Authentication</source>
+        <translation>Аутентификация</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="131"/>
+        <source>Authentication user was successful!</source>
+        <translation>Аутентификация нового пользователя прошла успешно!</translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationForm</name>
+    <message>
+        <location filename="../sources/registrationform.ui" line="20"/>
+        <source>Form</source>
+        <translation>Форма регистрации</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="28"/>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="38"/>
+        <source>Login:</source>
+        <translation>Логин:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="48"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="62"/>
+        <source>Confirm password:</source>
+        <translation>Подтвердить пароль:</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.ui" line="107"/>
+        <source>Login</source>
+        <translation>Вход</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="39"/>
+        <source>Passwords don&apos;t match. Try again.</source>
+        <translation>Пароли не совпадают. Попробуйте еще раз.</translation>
+    </message>
+    <message>
+        <location filename="../sources/registrationform.cpp" line="46"/>
+        <source>All fields must be filled in!</source>
+        <translation>Все поля должны быть заполнены!</translation>
+    </message>
+</context>
+<context>
+    <name>StartScreen</name>
+    <message>
+        <location filename="../sources/startscreen.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="30"/>
+        <source>Connect to Server</source>
+        <translation>Подключение к серверу</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="46"/>
+        <source>Login to Server</source>
+        <translation>Вход на сервер</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="52"/>
+        <source>New Registration</source>
+        <translation>Новая регистрация</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="86"/>
+        <location filename="../sources/startscreen.cpp" line="93"/>
+        <location filename="../sources/startscreen.cpp" line="108"/>
+        <source>User with login </source>
+        <translation>Пользователь с логином </translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="87"/>
+        <source> not added in database! Registration failed.</source>
+        <translation> не добавлен в базу данных! Регистрация не удалась.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="94"/>
+        <source> add in database! Registration success</source>
+        <translation> добавлен в базу данных! Регистрация успешна.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="108"/>
+        <source> is already exists! Try again.</source>
+        <translation> уже существует! Попробуйте ещё раз.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="113"/>
+        <source>The login must contain letters and numbers only!</source>
+        <translation>Логин должен содержать только буквы и цифры!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="116"/>
+        <source>Login &apos;all&apos; is reserved!</source>
+        <translation>Логин &apos;all&apos; зарезервирован!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="119"/>
+        <source>Password must be equal to or more than 5 characters long!</source>
+        <translation>Длина пароля должна быть равна или более 5 символов!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="122"/>
+        <source>Password must not exceed 20 characters long!</source>
+        <translation>Длина пароля не должна превышать 20 символов!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="125"/>
+        <source>Password contains invalid characters!</source>
+        <translation>Пароль содержит недопустимые символы!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="128"/>
+        <source>Authentication was successful.</source>
+        <translation>Аутентификация прошла успешно.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="139"/>
+        <source>Authentication was failed.</source>
+        <translation>Аутентификация завершилась неудачей.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="142"/>
+        <source>Invalid login! Try again, please.</source>
+        <translation>Неверный логин! Попробуйте еще раз.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="145"/>
+        <source>Invalid password! Try again, please.</source>
+        <translation>Неверный пароль! Попробуйте еще раз.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="148"/>
+        <source>Authentication failed! You&apos;ve used all attempts!</source>
+        <translation>Аутентификация не удалась! Вы использовали все попытки!</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="151"/>
+        <source>You are already online.</source>
+        <translation>Вы уже в сети.</translation>
+    </message>
+    <message>
+        <location filename="../sources/startscreen.cpp" line="167"/>
+        <source>All fields must be filled in.</source>
+        <translation>Все поля должны быть заполнены.</translation>
+    </message>
+</context>
+</TS>
