2753853a8b5ca5481182a90b87592bc926f33143
kab1201 <125040829+kab1201@users.noreply.github.com>
2024-01-08 21:56:18 +0300

    Add files via upload

---
+++ Server/qsqltablemodel_users.cpp
@@ -0,0 +1,7 @@
+#include "qsqltablemodel_users.h"
+
+QSqlTableModel_Users::QSqlTableModel_Users(QObject *parent)
+    : QSqlTableModel{parent}
+{
+
+}
---
+++ Server/qsqltablemodel_users.h
@@ -0,0 +1,12 @@
+#ifndef QSQLTABLEMODEL_USERS_H
+#define QSQLTABLEMODEL_USERS_H
+
+#include <QSqlTableModel>
+
+class QSqlTableModel_Users : public QSqlTableModel
+{
+public:
+    explicit QSqlTableModel_Users(QObject *parent = nullptr);
+};
+
+#endif // QSQLTABLEMODEL_USERS_H
