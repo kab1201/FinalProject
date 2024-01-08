9be8b7feef245d0a0957aa26ab0ebb5b8c43c82f
kab1201 <125040829+kab1201@users.noreply.github.com>
2024-01-08 21:55:37 +0300

    Add files via upload

---
+++ Server/translations/my_ru.ts
@@ -0,0 +1,297 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Чат (сервер)</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="36"/>
+        <source>Port:</source>
+        <translation>Порт:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="65"/>
+        <location filename="../sources/mainwindow.cpp" line="457"/>
+        <location filename="../sources/mainwindow.cpp" line="499"/>
+        <source>Connect</source>
+        <translation>Подключение</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="85"/>
+        <source>Filter users:</source>
+        <translation>Фильтр пользователей:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="108"/>
+        <location filename="../sources/mainwindow.cpp" line="74"/>
+        <location filename="../sources/mainwindow.cpp" line="594"/>
+        <location filename="../sources/mainwindow.cpp" line="629"/>
+        <location filename="../sources/mainwindow.cpp" line="645"/>
+        <source>Ban user</source>
+        <translation>Забанить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="115"/>
+        <source>Disconnect user</source>
+        <translation>Отключить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="139"/>
+        <source>Messages between users:</source>
+        <translation>Сообщения между пользователями:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="148"/>
+        <source>From:</source>
+        <translation>От кого:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="179"/>
+        <source>To:</source>
+        <translation>Кому:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="221"/>
+        <source>Show</source>
+        <translation>Показать</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="249"/>
+        <source>Logs:</source>
+        <translation>Информационные сообщения:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.ui" line="284"/>
+        <source>Shut down</source>
+        <translation>Выключить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="36"/>
+        <source>The Connection to the database was successful!</source>
+        <translation>Подключение к базе данных прошло успешно!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="40"/>
+        <source>Database Tables successfully found or created.</source>
+        <translation>Таблицы базы данных успешно найдены или созданы.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="46"/>
+        <source>The search or creation of database tables failed.</source>
+        <translation>Найти или создать таблицы базы данных не удалось.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="57"/>
+        <source>The Connection to the database failed!</source>
+        <translation>Подключение к базе данных не удалось!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="70"/>
+        <location filename="../sources/mainwindow.cpp" line="81"/>
+        <location filename="../sources/mainwindow.cpp" line="630"/>
+        <source>Unban user</source>
+        <translation>Разбанить</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="140"/>
+        <location filename="../sources/mainwindow.cpp" line="146"/>
+        <location filename="../sources/mainwindow.cpp" line="173"/>
+        <location filename="../sources/mainwindow.cpp" line="179"/>
+        <location filename="../sources/mainwindow.cpp" line="190"/>
+        <location filename="../sources/mainwindow.cpp" line="196"/>
+        <location filename="../sources/mainwindow.cpp" line="202"/>
+        <location filename="../sources/mainwindow.cpp" line="219"/>
+        <source> User with login &apos;</source>
+        <translation> Пользователь с логином &apos;</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="140"/>
+        <source>&apos; not added in database! Registration failed...</source>
+        <translation>&apos; не добавлен в базу данных! Регистрация не удалась...</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="146"/>
+        <source>&apos; add in database! Registration success...</source>
+        <translation>&apos; добавлена в базу данных! Регистрация успешна...</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="173"/>
+        <source>&apos; is already exists</source>
+        <translation>&apos; уже существует</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="179"/>
+        <source>&apos; not added in database! The login must contain letters and numbers only!</source>
+        <translation>&apos; не добавлен в базу данных! Логин должен содержать только буквы и цифры!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="185"/>
+        <source> Login &apos;all&apos; is reserved!</source>
+        <translation> Логин &apos;all&apos; зарезервирован!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="190"/>
+        <source>&apos; not added in database! Password must be equal to or more than 5 characters long!</source>
+        <translation>&apos; не добавлен в базу данных! Длина пароля должна быть равна или более 5 символов!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="196"/>
+        <source>&apos; not added in database! Password must not exceed 20 characters long!</source>
+        <translation>&apos; не добавлен в базу данных! Длина пароля не должна превышать 20 символов!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="202"/>
+        <source>&apos; not added in database! Password contains invalid characters!</source>
+        <translation>&apos; не добавлен в базу данных! Пароль содержит недопустимые символы!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="219"/>
+        <source>&apos; have made </source>
+        <translation>&apos; сделал </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="219"/>
+        <source> attempts to login!</source>
+        <translation> попытки залогиниться!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="229"/>
+        <source> Invalid login!</source>
+        <translation> Неверный логин!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="234"/>
+        <source> Invalid password for User with login &apos;</source>
+        <translation> Неверный пароль для пользователя с логином &apos;</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="251"/>
+        <location filename="../sources/mainwindow.cpp" line="271"/>
+        <location filename="../sources/mainwindow.cpp" line="277"/>
+        <source> The user with login &apos;</source>
+        <translation> Пользователь с логином &apos;</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="251"/>
+        <source>&apos; was authenticated</source>
+        <translation>&apos; аутентифицирован</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="271"/>
+        <source>&apos; is already online</source>
+        <translation>&apos; уже нолайн</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="277"/>
+        <source>&apos; was not authenticated</source>
+        <translation>&apos; не аутентифицирован</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="287"/>
+        <source>The user with login </source>
+        <translation>Пользователь с логином </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="366"/>
+        <source>New client connect from port number </source>
+        <translation>Новый клиент подключился с порта номер </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="390"/>
+        <location filename="../sources/mainwindow.cpp" line="688"/>
+        <source>Client from port number </source>
+        <translation>Клиент с порта номер </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="418"/>
+        <source>Requested command from client: Registration.</source>
+        <translation>Запрошена команда от клиента: Регистрация.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="423"/>
+        <source>Requested command from client: Login.</source>
+        <translation>Запрошена команда от клиента: Логин.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="428"/>
+        <source>Requested command from client: Send message to all.</source>
+        <translation>Запрошена команда от клиента: Отправить сообщение всем.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="430"/>
+        <source>write:</source>
+        <translation>пишет:</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="436"/>
+        <source>Requested command from client: Send private message.</source>
+        <translation>Запрошена команда от клиента: Отправить сообщение.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="441"/>
+        <source>Requested command from client: Get chat between two users.</source>
+        <translation>Запрошена команда от клиента: Получить чат между двумя пользователями.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="446"/>
+        <source>Requested command from client: Logout.</source>
+        <translation>Запрошена команда от клиента: Выход.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="468"/>
+        <source>Disconnect</source>
+        <translation>Отключение</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="522"/>
+        <source>No messages between users!</source>
+        <translation>Нет сообщений между пользователями!</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="539"/>
+        <location filename="../sources/mainwindow.cpp" line="563"/>
+        <location filename="../sources/mainwindow.cpp" line="568"/>
+        <source>write to</source>
+        <translation>пишет</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="638"/>
+        <location filename="../sources/mainwindow.cpp" line="649"/>
+        <source>Client with login </source>
+        <translation>Клиент с логином </translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="640"/>
+        <source>sent to ban.</source>
+        <translation>отправлен в бан.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="651"/>
+        <source>unban.</source>
+        <translation>разбанен.</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="690"/>
+        <source>disconnect.</source>
+        <translation>отключен.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="52"/>
+        <location filename="../sources/mainwindow.cpp" line="463"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="53"/>
+        <source>Connection with database failed!</source>
+        <translation>Соединение с базой данных не удалось!</translation>
+    </message>
+</context>
+</TS>
